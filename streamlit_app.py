27ff58ba52c604a61da4b1d479cae6ea1fc9b677
Gitcreeeed <sanduck85@gmail.com>
2022-11-08 12:41:22 +0900

    check_box

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,12 @@ if st.button("click button"):
       st.write("여기엔 아무것도 없다..")
       # 데이터 로딩 함수는 여기에!
 
+# 체크박스 넣기
+checkbox_btn = st.checkbox('조건에 해당되면 누르세요')
+	
+  if checkbox_btn:
+      st.write('Great!')
+
 # 첫번째 행
 r1_col1, r1_col2, r1_col3 = st.columns(3)
 
